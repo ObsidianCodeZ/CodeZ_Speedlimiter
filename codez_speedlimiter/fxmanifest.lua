cc2f638ef5b35e8fdc264d7f0d1eab729ee3cf2b
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-10 11:14:39 +0200

    Add files via upload

---
+++ codez_speedlimiter/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+name "CodeZ_Speedlimiter"
+author "Obsidian_CodeZ"
+description 'Speed Limiter Script By Obsidian_CodeZ'
+lua54 "yes"
+
+client_scripts {
+    "client.lua",
+}
+
+server_scripts {
+    "server.lua",
+}
+
