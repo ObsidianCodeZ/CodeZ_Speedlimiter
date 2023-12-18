81c60c47b7b9c600460ba6b928fe7d6957c53165
Obsidian_CodeZ <153365250+ObsidianCodeZ@users.noreply.github.com>
2023-12-19 01:47:07 +0200

    Update fxmanifest.lua

--- codez_speedlimiter/fxmanifest.lua
+++ codez_speedlimiter/fxmanifest.lua
@@ -3,6 +3,7 @@ game 'gta5'
 name "CodeZ_Speedlimiter"
 author "Obsidian_CodeZ"
 description 'Speed Limiter Script By Obsidian_CodeZ'
+contact 'https://discord.com/invite/8nQcNAYTju'
 lua54 "yes"
 
 client_scripts {
